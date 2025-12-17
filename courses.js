01dbbad5d9295e0c3a632201774fe97aeb3a431c
htseng64 <huichentseng@gmail.com>
2025-12-17 18:33:01 +0800

    Add courses data file

---
+++ courses.js
@@ -0,0 +1,42 @@
+const courses = [
+  {
+    id: "med-surg-nursing",
+    name: "內外科護理學",
+    stroke: 4
+  },
+  {
+    id: "intro-to-nursing",
+    name: "護理學導論",
+    stroke: 20
+  },
+  {
+    id: "evidence-based-nursing",
+    name: "研究及實證護理概論",
+    stroke: 9
+  },
+  {
+    id: "bioethics",
+    name: "醫療與生命倫理",
+    stroke: 18
+  },
+  {
+    id: "healthcare-ethics-law",
+    name: "醫護倫理與法規概論",
+    stroke: 18
+  },
+  {
+    id: "clinical-symptom-nursing",
+    name: "臨床症狀護理",
+    stroke: 17
+  },
+  {
+    id: "professional-english-presentations",
+    name: "職場專業英文簡報",
+    stroke: 18
+  },
+  {
+    id: "climate-health-nursing-practicum",
+    name: "護理視角下的氣候健康挑戰與調適（實作）",
+    stroke: 20
+  }
+];
